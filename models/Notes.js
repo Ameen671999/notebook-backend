b6a3853e5ecd0e379f9f81ed698b24a55eee3c3b
Ameen671999 <ameen671999@gmail.com>
2021-10-27 12:51:11 +0530

    update and delete notes

--- models/Notes.js
+++ models/Notes.js
@@ -1,25 +1,29 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
-  const notesSchema = new Schema({
-      title: {
-          type: String,
-          required: true
-      },
-      description: {
-        type: String,
-        required: true,
-        default: 'General'
-    },
-      tag: {
-        type: String,
-        default: 'general'
-    },
-    date: {
-        type: String,
-        default: Date.now
-    },
-  });
+const notesSchema = new Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    default: "General",
+  },
+  tag: {
+    type: String,
+    default: "general",
+  },
+  date: {
+    type: String,
+    default: Date.now,
+  },
+});
 
-  module.exports = mongoose.model('notes', notesSchema)
+module.exports = mongoose.model("notes", notesSchema);
--- routes/notes.js
+++ routes/notes.js
@@ -50,4 +50,70 @@ router.post(
   }
 );
 
+//ROUTE 3: update an existing note using: PUT: 'api/auth/updatenote'
+router.put("/updatenote/:id", fetchUser, async (req, res) => {
+  try {
+    const { title, description, tag } = req.body;
+  const newNote = {};
+  if (title) {
+    newNote.title = title;
+  }
+  if (description) {
+    newNote.description = description;
+  }
+  if (tag) {
+    newNote.tag = tag;
+  }
+
+  //Find the note to be updated and update it
+  let note = await Notes.findById(req.params.id);
+  if (!note) {
+    return res.status(404).send("Not Found");
+  }
+  //relevent user
+  if (note.user.toString() !== req.user.id) {
+    return res.status(401).send("Not Allowed");
+  }
+
+  note = await Notes.findByIdAndUpdate(
+    req.params.id,
+    { $set: newNote },
+    { new: true }// new contact will be created
+  );
+  res.json({note})
+  }catch (error) {
+    console.log(error.message);
+    res.status(500).send("User some error occured");
+    //put in logger and sqs
+  }
+  
+});
+
+
+//ROUTE 3: delete an existing note using: delete: 'api/auth/deletenote'
+router.delete("/deletenote/:id", fetchUser, async (req, res) => {
+  try {
+      
+  //Find the note to be updated and deleted
+  let note = await Notes.findById(req.params.id);
+  if (!note) {
+    return res.status(404).send("Not Found");
+  }
+  //relevent user 
+  if (note.user.toString() !== req.user.id) {
+    return res.status(401).send("Not Allowed");
+  }
+
+  note = await Notes.findByIdAndDelete(
+    req.params.id
+  );
+  res.json("Success note has been deleted")
+  }catch (error) {
+    console.log(error.message);
+    res.status(500).send("User some error occured");
+    //put in logger and sqs
+  }
+
+});
+
 module.exports = router;
